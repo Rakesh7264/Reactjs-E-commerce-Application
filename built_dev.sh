51e9e51dcffc75be5b71677412bff2812d16ae03
Ubuntu <ubuntu@ip-172-31-20-227.eu-central-1.compute.internal>
2026-04-04 13:17:01 +0000

    Docker files

---
+++ built_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE_NAME=devops-app
+TAG=latest
+
+echo "Building Docker Image"
+docker build -t $IMAGE_NAME:$TAG .
+
+echo "Tagging for DockerHub"
+docker tag $IMAGE_NAME:$TAG rakesh7264/dev:$TAG
+
+echo "Pushing to DockerHub DEV repo"
+docker push rakesh7264/dev:$TAG
+
+echo "Build completed!"
