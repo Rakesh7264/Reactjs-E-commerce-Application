9b006f451dc26e1129846c553131fe9a11240c22
Ubuntu <ubuntu@ip-172-31-20-227.eu-central-1.compute.internal>
2026-04-04 13:32:03 +0000

    Docker files

---
+++ deploy_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE=rakesh7264/dev:latest
+
+echo "Pulling latest image..."
+docker pull $IMAGE
+
+echo "Stopping old container..."
+docker stop devops-app || true
+docker rm devops-app || true
+
+echo "Running new container..."
+docker run -d -p 80:80 --name devops-app $IMAGE
+
+echo "Deployment completed!"
