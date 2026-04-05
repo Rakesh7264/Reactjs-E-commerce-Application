dcae4bb76e5267b30258098fada81fbd67a1c5e2
Rakesh7264 <rakeelango@gmail.com>
2026-04-05 18:27:43 +0530

    Update deploy_dev.sh

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -5,7 +5,7 @@ IMAGE=rakesh7264/dev:latest
 echo "Pulling latest image.."
 docker pull $IMAGE
 
-echo "Stopping old container..."
+echo "Stopping old container.."
 docker stop devops-app || true
 docker rm devops-app || true
 
